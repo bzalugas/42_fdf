049445e50b98e70613466e0499b48c1637153014
bazaluga <bazaluga@student.42.fr>
2024-06-07 12:41:06 +0200

    feat: Update libft

--- include/libft/ft_ftoa.c
+++ include/libft/ft_ftoa.c
@@ -6,11 +6,11 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 12:32:20 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/06/06 19:26:12 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/06/07 12:31:07 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 static size_t	n_size(long int_part)
 {
@@ -25,10 +25,24 @@ static size_t	n_size(long int_part)
 	return (size);
 }
 
+static void	round_str(char *res, double remainder, size_t end)
+{
+	if ((int)(remainder * 10) >= 5)
+		res[end]++;
+	while (end > 0 && res[end] != '.' && res[end] > '9')
+	{
+		res[end] = '0';
+		res[end - 1]++;
+		end--;
+	}
+}
+
 static void	convert(char *res, double n, unsigned long int_part, size_t *sizes)
 {
 	size_t	i;
+	size_t	len;
 
+	len = sizes[0];
 	i = sizes[0] - sizes[1];
 	n -= int_part;
 	while (sizes[0] > 0 && n > 0.0 && sizes[1] > 0)
@@ -39,14 +53,16 @@ static void	convert(char *res, double n, unsigned long int_part, size_t *sizes)
 		i++;
 		sizes[1]--;
 		sizes[0]--;
+		if (sizes[1] == 0)
+			res[--sizes[0]] = '.';
 	}
-	res[--sizes[0]] = '.';
-	while (sizes[0] > 0)
+	while (sizes[0] > 0 && int_part > 0)
 	{
 		res[sizes[0] - 1] = int_part % 10 + '0';
 		int_part /= 10;
 		sizes[0]--;
 	}
+	round_str(res, n, len - 1);
 }
 
 char	*ft_ftoa(double n, int precision)
@@ -56,14 +72,16 @@ char	*ft_ftoa(double n, int precision)
 	size_t	sizes[2];
 	char	*res;
 
+	if (precision < 0)
+		return (NULL);
 	sizes[1] = precision;
 	neg = 0;
 	if (n < 0 && ++neg)
 		n *= -1;
 	int_part = (unsigned long)n;
 	sizes[0] = n_size(int_part);
-	sizes[0] += neg + precision + 1;
-	res = (char *)calloc(sizes[0] + 1, sizeof(char));
+	sizes[0] += neg + precision + (1 * (precision > 0));
+	res = (char *)ft_calloc(sizes[0] + 1, sizeof(char));
 	if (!res)
 		return (NULL);
 	convert(res, n, int_part, sizes);
